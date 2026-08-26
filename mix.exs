abbbbf13cf54bd0b3cfc47f47c8df3d0224bcc1f
Darwin Wu <tough.pasta0068@wunli88.com>
2026-08-26 00:47:31 +0000

    chore(release): synchronize project version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Kodo.MixProject do
   def project do
     [
       app: :kodo,
-      version: "0.0.1",
+      version: "0.1.0",
       elixir: "~> 1.20",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
